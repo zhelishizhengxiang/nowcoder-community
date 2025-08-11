1c6c6473bea2c3e21a3e2697b62ee6d66142d762
zhelishizhengxiang <2114369430@qq.com>
2025-08-11 21:49:06 +0800

    'first commit'

--- community/src/main/java/com/nowcoder/community/dao/AlphaDAO.java
+++
@@ -1,9 +0,0 @@
-package com.nowcoder.community.dao;
-
-/**
- * @author zhengx
- * @version 1.0
- */
-public interface AlphaDAO {
-    String select();
-}
---
+++ community/src/main/java/org/simom/community/config/AlphaConfig.java
@@ -0,0 +1,19 @@
+package org.simom.community.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * @author zhengx
+ * @version 1.0
+ */
+@Configuration
+public class AlphaConfig {
+
+    @Bean
+    public SimpleDateFormat simpleDateFormat() {
+        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    }
+}
